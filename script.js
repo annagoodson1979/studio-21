f7c6e10b36a02baa59892fb2d77601b60971f6a3
annagoodson1979 <annagoodson1979@gmail.com>
2026-07-02 22:54:58 -0500

    Add Studio 21 placeholder site

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const siteNav = document.querySelector(".site-nav");
+
+if (menuToggle && siteNav) {
+  menuToggle.addEventListener("click", () => {
+    siteNav.classList.toggle("is-open");
+  });
+}
+
+const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+document.querySelectorAll(".site-nav a").forEach((link) => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("is-active");
+  }
+});
