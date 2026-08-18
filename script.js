c5efd3c7918b27e6b89e07ce5a8fb6d91b58762c
annagoodson1979 <annagoodson1979@gmail.com>
2026-08-18 16:41:32 -0500

    Add Studio 21 Friends Family test code

--- script.js
+++ script.js
@@ -19,9 +19,13 @@ const clientPortalForm = document.querySelector("#clientPortalForm");
 const clientCodeInput = document.querySelector("#clientCode");
 const portalMessage = document.querySelector("#portalMessage");
 
+const existingClientBookingLink =
+  "https://www.vagaro.com/Users/BusinessWidget.aspx?enc=MMLjhIwJMcwFQhXLL7ifVNegKF6XEhkW8fsL0qaCKPKiwTc6lrPA+9bc+rNHn1y1m7JPw3aCiXY9ZpndUIRYZHY5aLzQ3tD/Emowzrjfqc/64IKxnyEPxwwDA59jK2K0dtbOfoYSEELZuDsODh82I302t5XqsR3R1uaf6o7hxCvwoDRqfp7Uxqv/0/wx/88R76XmrgYE58EUZ42ECO38Eo8XGACY+NSEBc6ROUnx0jPqy8jgkWABHUwgiMT7DHu/g/o5/hqPx64WNfefhI97wZoqgUi/LsrQ9ElF+kz6Dl+KC0lxrcbKMyyigoWacOkOodBCJg5mumnvmp9aZ+DDGNScr1BUpTPRJZVt4LSn9OM0aMc6xXWIcW1wvWm62AORmH4j4MES7nKO96NZDRgfF5gm3UAjbDLdAcFn13Y1ZVjeu9nZGdnjqc8TPa/d2sYzGhMpgFcgb3ubFEx/jYIsVv/VYrFJItkUK2DKhqwl3kg=";
+
 const clientPortalLinks = {
-  "NGU-A": "https://www.vagaro.com/Users/BusinessWidget.aspx?enc=MMLjhIwJMcwFQhXLL7ifVNegKF6XEhkW8fsL0qaCKPKiwTc6lrPA+9bc+rNHn1y1m7JPw3aCiXY9ZpndUIRYZHY5aLzQ3tD/Emowzrjfqc/64IKxnyEPxwwDA59jK2K0dtbOfoYSEELZuDsODh82I302t5XqsR3R1uaf6o7hxCvwoDRqfp7Uxqv/0/wx/88R76XmrgYE58EUZ42ECO38Eo8XGACY+NSEBc6ROUnx0jPqy8jgkWABHUwgiMT7DHu/g/o5/hqPx64WNfefhI97wZoqgUi/LsrQ9ElF+kz6Dl+KC0lxrcbKMyyigoWacOkOodBCJg5mumnvmp9aZ+DDGNScr1BUpTPRJZVt4LSn9OM0aMc6xXWIcW1wvWm62AORmH4j4MES7nKO96NZDRgfF5gm3UAjbDLdAcFn13Y1ZVjeu9nZGdnjqc8TPa/d2sYzGhMpgFcgb3ubFEx/jYIsVv/VYrFJItkUK2DKhqwl3kg=",
-  "NGU-A21": "https://www.vagaro.com/Users/BusinessWidget.aspx?enc=MMLjhIwJMcwFQhXLL7ifVNegKF6XEhkW8fsL0qaCKPKiwTc6lrPA+9bc+rNHn1y1m7JPw3aCiXY9ZpndUIRYZHY5aLzQ3tD/Emowzrjfqc/64IKxnyEPxwwDA59jK2K0dtbOfoYSEELZuDsODh82I302t5XqsR3R1uaf6o7hxCvwoDRqfp7Uxqv/0/wx/88R76XmrgYE58EUZ42ECO38Eo8XGACY+NSEBc6ROUnx0jPqy8jgkWABHUwgiMT7DHu/g/o5/hqPx64WNfefhI97wZoqgUi/LsrQ9ElF+kz6Dl+KC0lxrcbKMyyigoWacOkOodBCJg5mumnvmp9aZ+DDGNScr1BUpTPRJZVt4LSn9OM0aMc6xXWIcW1wvWm62AORmH4j4MES7nKO96NZDRgfF5gm3UAjbDLdAcFn13Y1ZVjeu9nZGdnjqc8TPa/d2sYzGhMpgFcgb3ubFEx/jYIsVv/VYrFJItkUK2DKhqwl3kg=",
+  "NGU-A": existingClientBookingLink,
+  "NGU-A21": existingClientBookingLink,
+  "MIL-C": existingClientBookingLink,
   TEST21: "salon.html#booking",
   NEW21: "https://docs.google.com/forms/d/e/1FAIpQLSfx-d8XV8dK5MV7Ipuv1ZPb2tjcGKTUJ5n0QCYOgZIVgET7gw/viewform?usp=header",
   VIP21: "https://www.vagaro.com/cl/g0DZI7~ydq9D9PoeHO2L0xx1zh7~fzj81vUZ17IACbQ=",
